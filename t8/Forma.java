d7b51827a977fc22406bd251f1052ec9d710943c
AlanBessauer <ablencina@inf.ufsm.br>
2016-06-01 23:55:27 -0300

    Create Forma.java

---
+++ t8/Forma.java
@@ -0,0 +1,12 @@
+package t8;
+
+/**
+ *
+ * @author
+ */
+public abstract class Forma {
+    
+    protected Ponto[] pontos;
+    public abstract float getArea();
+    public abstract Ponto getPonto();
+}
